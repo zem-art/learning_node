86bbbc8ede6a6e10e32a64c5f74f9a3484981b77
Zaid <zaid@email.com>
2021-10-10 08:32:22 -0400

    Remake Coding and Finish Aplication

--- 6.Membuat_Contac_App/app.js
+++ 6.Membuat_Contac_App/app.js
@@ -5,38 +5,81 @@ const contect = require("./contect");
 
 // // Contoh Yargs Dengan Parameter Banyak Menggunakan Object
 
+yargs
+  .command({
+    command: "add", // Perintah
+    describe: "Menambahkan Contact Baru", // Describsi
+    // Fields Yang Di Inginkan nya
+    builder: {
+      nama: {
+        describe: "Nama Lengkap",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "Email Anda",
+        demandOption: false,
+        type: "string",
+      },
+      NoHp: {
+        describe: "NoHp Anda",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    // untuk mengeksekusi fungsinya yang sudah di buat
+    handler(argv) {
+      // Menyatukan dengan File contect dan data nya akan di simpan di file JSON
+      contect.SaveData(argv.nama, argv.email, argv.NoHp);
+
+      // const data = {
+      //   nama: argv.nama,
+      //   email: argv.email,
+      //   noHp: argv.noHp,
+      // };
+      // console.log(data);
+    },
+  })
+  .demandCommand(); // untuk Menambahkan Warning
+
+// Melihat Semua Data Di Dalam Contact
 yargs.command({
-  command: "add", // Perintah
-  describe: "Menambahkan Contact Baru", // Describsi
-  // Fields Yang Di Inginkan nya
+  command: "list", // Perintah
+  describe: "Melihat List Contact",
+  handler() {
+    contect.listContact();
+  },
+});
+
+// Melihat Detail Contact Berdasarkan Nama
+yargs.command({
+  command: "detail",
+  describe: "Detail Sebuah Contact Berdasarkan Nama",
   builder: {
     nama: {
       describe: "Nama Lengkap",
       demandOption: true,
       type: "string",
     },
-    email: {
-      describe: "Email Anda",
-      demandOption: false,
-      type: "string",
-    },
-    noHp: {
-      describe: "NoHp Anda",
+  },
+  handler(argv) {
+    contect.detailContact(argv.nama);
+  },
+});
+
+// Menghapus Contact Berdasarkan Nama
+yargs.command({
+  command: "delete",
+  describe: "Menghapus Contact Berdasarkan Nama",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap",
       demandOption: true,
       type: "string",
     },
   },
-  // untuk mengeksekusi fungsinya yang sudah di buat
   handler(argv) {
-    // Menyatukan dengan File contect dan data nya akan di simpan di file JSON
-    contect.SaveData(argv.nama, argv.email, argv.noHp);
-
-    // const data = {
-    //   nama: argv.nama,
-    //   email: argv.email,
-    //   noHp: argv.noHp,
-    // };
-    // console.log(data);
+    contect.deleteContact(argv.nama);
   },
 });
 
--- 6.Membuat_Contac_App/contect.js
+++ 6.Membuat_Contac_App/contect.js
@@ -40,16 +40,25 @@ if (!fs.existsSync(dirFile)) {
 //   });
 // };
 
-const SaveData = (nama, email, NoHp) => {
-  const data = { nama, email, NoHp };
-
+const loadContact = () => {
+  // // Fungsi Untuk Membaca File JSON nya
   const readFile = fs.readFileSync("./data/context.json");
 
   const changeData = JSON.parse(readFile);
 
+  return changeData;
+};
+
+const SaveData = (nama, email, NoHp) => {
+  const data = { nama, email, NoHp };
+  // const readFile = fs.readFileSync("./data/context.json");
+  // const changeData = JSON.parse(readFile);
+
+  const readDataJson = loadContact(); // Memisahkan Fungsi Data Json Dan Mengubah Nya Menjadi Fungis Yang bisa di Pakai Terus Menerus
+
   // // CEK NAMA DUPLIKAT
   // // Cek Apakah Ada Nama Duplikat Di dalam Contact
-  const duplikatName = changeData.find((data) => data.nama === nama); // data.nama => data yang ada di dalam file
+  const duplikatName = readDataJson.find((data) => data.nama === nama); // data.nama => data yang ada di dalam file
   if (duplikatName) {
     console.log(
       chalk.blue.inverse.bold("Contact Sudah Terdaftar Gunakan Nama Lain !")
@@ -68,13 +77,13 @@ const SaveData = (nama, email, NoHp) => {
 
   // // CEK NO HP
   if (!validator.isMobilePhone(NoHp, "id-ID")) {
-    console.log(chalk.blue.inverse.bold("No Phone Tidak Validc !"));
+    console.log(chalk.blue.inverse.bold("No Phone Tidak Valid !"));
     return false;
   }
 
-  changeData.push(data);
+  readDataJson.push(data);
 
-  fs.writeFileSync("./data/context.json", JSON.stringify(changeData));
+  fs.writeFileSync("./data/context.json", JSON.stringify(readDataJson));
 
   console.log(
     chalk.green.inverse.bold("Terimakasih Sudah Memasukan Data Nya ^_^ ")
@@ -83,4 +92,59 @@ const SaveData = (nama, email, NoHp) => {
   //   rl.close(); // sudah tidak di gunakan karna sudah menggunakan yargs
 };
 
-module.exports = { SaveData };
+const listContact = () => {
+  const readDataJson = loadContact();
+
+  console.log(chalk.blue.inverse.bold("Daftar Contact Anda :"));
+
+  readDataJson.forEach((data, i) => {
+    // Data Nya Di For Menggunakan ForEach Dan Di simpan Di parameter data dan id nya di i
+    console.log(`${i + 1} . ${data.nama} - ${data.NoHp}`);
+  });
+};
+
+const detailContact = (nama) => {
+  const readDataJson = loadContact();
+
+  const FindData = readDataJson.find(
+    (readDataJson) => readDataJson.nama.toLowerCase() === nama.toLowerCase() // Mencari Nama Di dalam var dataJson
+  ); // toLowerCase => itu tidak mempentingkan huruf kecil dan besar di dalam penulisan nya
+
+  if (!FindData) {
+    // Apabila Tidak Di temukan Datanya
+    console.log(chalk.red.inverse.bold(`${nama} , Tidak Ditemukan !!`));
+    return false;
+  }
+
+  console.log(chalk.blue.inverse.bold("Contact Di Temukan"));
+  console.log(`Nama : ${FindData.nama}`);
+  console.log(`No Hp : ${FindData.noHp}`);
+  if (FindData.email) {
+    console.log(chalk.blue.inverse.bold(`Email : ${FindData.email}`));
+    return false;
+  }
+};
+
+const deleteContact = (nama) => {
+  // Array lama
+  const readDataJson = loadContact();
+  // array baru yang nanti nya untuk mendelete berdasarkan nama , kenapa menggunakan filter karna filter tidak akan berhenti walupun sudah di temukan datanya
+  const newDataJson = readDataJson.filter(
+    (contact) => contact.nama.toLowerCase() !== nama.toLowerCase()
+  );
+  // Apabila Data Yang Di masukan sama apa tidak Dengan Data Yang Di dalam file.jsonnya
+  if (readDataJson.length === newDataJson.length) {
+    // Apabila Tidak Di temukan Datanya
+    console.log(chalk.red.inverse.bold(`${nama} , Tidak Ditemukan !!`));
+    return false;
+  }
+
+  // APabila Berhasil Di temukan
+  fs.writeFileSync("./data/context.json", JSON.stringify(newDataJson));
+
+  console.log(
+    chalk.green.inverse.bold(`Data Contact ${nama} , Berhasil Dihapus`)
+  );
+};
+
+module.exports = { SaveData, listContact, detailContact, deleteContact };
